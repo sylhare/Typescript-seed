6de2f06a2fa349d82edfa8eecee33e383af925c3
sylhare <sylhare@outlook.com>
2024-03-04 09:14:58 -0500

    Add Typescript seed

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: "ts-jest",
+    testRegex: "\\.(test|e2e)\\.ts$",
+    moduleFileExtensions: ["ts", "js", "json"],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    setupFilesAfterEnv: [],
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.ts", "!src/index.ts"],
+    coveragePathIgnorePatterns: [],
+    coverageReporters: ["text", "text-summary", "lcov"],
+    coverageThreshold: {
+        "global": {
+            "branches": 85,
+            "lines": 85
+        }
+    },
+};
+
+export default config;
